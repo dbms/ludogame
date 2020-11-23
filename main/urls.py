db7e72b1afbebc8733dddde1dd96ed87aa74fbe3
dbms <ideepakbalhara@gmail.com>
2020-11-23 23:28:22 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view()),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
