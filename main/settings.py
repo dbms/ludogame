0fbcb92ddfb1dd3766f0e71694d469c9bb3dbda1
dbms <ideepakbalhara@gmail.com>
2020-11-23 23:35:52 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -112,7 +112,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
